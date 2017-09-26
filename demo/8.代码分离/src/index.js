5ee14d9a126aaa2eb106d089204b91d5c0c7efd4
彭迅鹏 <1151957830@qq.com>
2017-09-26 10:37:49 +0800

    代码分离

---
+++ demo/8.代码分离/src/index.js
@@ -0,0 +1,29 @@
+// import _ from 'lodash';
+import printf from './printf.js';
+import { cube } from './math.js'
+import './style.css';
+
+var compont = () => {
+    var ele = document.createElement('div');
+    // ele.innerHTML = _.join(['Hello','webpack'], ' ');
+    ele.innerHTML = '2的立方：' + cube(2);
+    var btn = document.createElement('button');
+    btn.innerHTML = '点我后控制台有显示东西哦';
+    btn.onclick = printf;
+
+    ele.appendChild(btn);
+    return ele;
+}
+var element = compont();
+document.body.appendChild(element);
+
+// 热替换配置(只针对printf.js的模块替换)
+if (module.hot) {
+    module.hot.accept('./printf.js', () => {
+        console.log('updating...');
+        // 手动重新加载更新的代码
+        document.body.removeChild(element);
+        element = compont();
+        document.body.appendChild(element);
+    })
+}
---
+++ demo/8.代码分离/src/printf.js
@@ -0,0 +1,5 @@
+var printf = () => {
+    console.log('我是来自printf.js的代码');
+    console.log('updated1');
+}
+module.exports = printf;
